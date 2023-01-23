56b3d779fb02915905383c2e0487f543dbb5cfaf
maroneill <93486424+maroneill@users.noreply.github.com>
2023-01-22 16:26:12 -0800

    Create pytech_update.py

---
+++ module_6/pytech_update.py
@@ -0,0 +1,27 @@
+import pymongo
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://admin:admin@cluster0.ftrlqqg.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["pytech"]
+
+students = db["students"]
+
+
+
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for student in students.find():
+    print("Student ID: ", student["student_id"])
+    print("First Name: ", student["first_name"])
+    print("Last Name: ", student["last_name"])
+    print("\n")
+
+students.update_one({"student_id": 1007}, {"$set": {"last_name": "UPDATED"}})
+
+print("-- DISPLAYING STUDENT DOCUMENT 1007 --")
+student = students.find_one({"student_id": 1007})
+print("Student ID: ", student["student_id"])
+print("First Name: ", student["first_name"])
+print("Last Name: ", student["last_name"])
+
